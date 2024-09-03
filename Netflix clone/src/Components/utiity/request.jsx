b861a6745d53203ec6a7ef44c0e61731823ee941
mikias7890 <mikitemesgen939@gmail.com>
2024-09-03 10:42:55 +0200

    api key

---
+++ Netflix clone/src/Components/utiity/request.jsx
@@ -0,0 +1,11 @@
+const API_KEY = process.env.REACT_APP_API_KEY;
+const request = {
+  fetchTrending: `/movie/top_rated?api_key${API_KEY}language=en-US&page=1`,
+  fetchactionmovies: `/discover/movie?api_key=${API_KEY}&with_genre=28`,
+  fetchComedymovies: `/discover/movie?api_key=${API_KEY}&with_genre=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genre=2`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genre=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genre=99`,
+  fetchTopratedmovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+};
+export default request;
