92c8f86af703df108260d69696e2d56bd9ea34a8
mikias7890 <mikitemesgen939@gmail.com>
2024-09-11 23:15:07 +0200

    add

--- Netflix clone/src/Components/Footer/Footer.jsx
+++ Netflix clone/src/Components/Footer/Footer.jsx
@@ -6,50 +6,54 @@ import "./footer.css";
 
 function Footer() {
   return (
-    <div className="footer-container-one">
-      <div className="footer-container-two">
-        <div className="footer-icons">
-          <FacebookOutlinedIcon />
-          <InstagramIcon />
-          <YouTubeIcon />
-        </div>
-        <div className="footer-data ">
-          <div>
-            <ul>
-              <li> Audio description </li>
-              <li>Investor relation</li>
-              <li>Legal notice</li>
-              <li>Advert choice</li>
-            </ul>
+    <>
+      <div className="footer-container-one">
+        <div className="footer-container-two">
+          <div className="footer-icons">
+            <FacebookOutlinedIcon />
+            <InstagramIcon />
+            <YouTubeIcon />
           </div>
-          <div>
-            <ul>
-              <li>Help center</li>
-              <li>Jobs</li>
-              <li>Cookie preferance</li>
-            </ul>
+          <div className="footer-data ">
+            <div>
+              <ul>
+                <li> Audio description </li>
+                <li>Investor relation</li>
+                <li>Legal notice</li>
+                <li>Advert choice</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Help center</li>
+                <li>Jobs</li>
+                <li>Cookie preferance</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Gift card</li>
+                <li>Terms of use</li>
+                <li>Corporate information</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Media center</li>
+                <li>Privacy</li>
+                <li>Contact</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <li>Gift card</li>
-              <li>Terms of use</li>
-              <li>Corporate information</li>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <li>Media center</li>
-              <li>Privacy</li>
-              <li>Contact</li>
-            </ul>
-          </div>
-          <div className="service">
-            <p>service code</p>
-          </div>
-          <div className="copy">&copy;2024 Netflix</div>
         </div>
       </div>
-    </div>
+      <div className="second">
+        <div className="copy">&copy;2024 Netflix</div>
+        <div className="service">
+          <p>service code</p>
+        </div>
+      </div>
+    </>
   );
 }
 
--- Netflix clone/src/Components/Header/Header.jsx
+++ Netflix clone/src/Components/Header/Header.jsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./header.css";
 import Netflix_logo from "../../assets/Images/Netflix_Logo_RGB.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
 function Header() {
+  const [isHide, setIsHide] = useState(false);
+  //   A state variable isHide is declared using the useState hook and initialized to false.
+  // isHide will be used to determine whether the header should be hidden or not.
+  // setIsHide is the function used to update the state
+  useEffect(() => {
+    //     This hook is used to run side effects, such as setting up an IntersectionObserver.
+    // It runs once after the initial render because it has an empty dependency array ([]), meaning it will only execute when the component mounts.
+    // const target = document.querySelector('.element-to-observe');
+    const header = document.querySelector(".header");
+    const banner = document.querySelector(".banner");
+    console.log(banner);
+    //     document.querySelector is used to select two DOM elements:
+    // The element with the class .header (likely the header of the page).
+    // The element with the class .banner (probably an introductory banner).
+    // These elements will be observed or manipulated in the upcoming code
+    const oprCallback = function (entries) {
+      console.log(entries);
+      const [entry] = entries;
+      console.log(entry.isIntersecting);
+      setIsHide(() => !entry.isIntersecting);
+    };
+    //     This is the callback function that gets executed whenever the intersection status of the observed element (.banner) changes (i.e., whether the banner is in or out of view).
+    // entries is an array of intersection observer entries, but only the first entry is destructured with const [entry] = entries;.
+    // entry.isIntersecting indicates whether the element is currently intersecting (visible within the viewport).
+    // setIsHide(() => !entry.isIntersecting):
+    // This line updates the isHide state. If the banner is not intersecting (i.e., no longer in view), isHide is set to true, hiding the header. If the banner is intersecting (visible), isHide is set to false.
+    const option = {
+      root: null,
+      // rootMargin: '0px',
+      threshold: 0.5,
+    };
+    const observe = new IntersectionObserver(oprCallback, option);
+    observe.observe(banner);
+    //     Options (option):
+    // root: null: This means the root is the browser viewport.
+    // threshold: 0.5: This specifies that the callback will be triggered when 50% of the banner is visible in the viewport.
+    // IntersectionObserver (observe):
+    // A new IntersectionObserver instance is created with oprCallback as the callback function and option as its configuration.
+    // The observe.observe(banner) line starts observing the banner element to detect when it enters or exits the viewport
+  }, []);
+
   return (
     <div className="header">
-      <div className="header-container">
+      <div className={`header-container ${isHide ? "sticky" : ""}`}>
         <div className="right">
           <ul>
             <li>
